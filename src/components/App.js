4b51e6233f0c34049502662697c7865c723bb08c
Benard Khajira <benardkhajira@gmail.com>
2024-05-26 01:01:45 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React from "react";
-import blogData from "../data/blog";
+import React from 'react';
+import ArticleList from 'src/components/ArticleList';
 
-console.log(blogData);
+const posts = [
+  {
+    title: 'Components 101',
+    date: 'December 15, 2020',
+    preview: 'Setting up the building blocks of your site',
+  },
+  {
+    title: 'React Data Flow',
+    date: 'December 11, 2020',
+    preview: 'Passing props is never passé',
+  },
+  {
+    title: 'Function Components vs Class Components',
+    date: 'December 10, 2020',
+    preview: 'React, meet OOJS.',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <ArticleList posts={posts} />
     </div>
   );
 }
